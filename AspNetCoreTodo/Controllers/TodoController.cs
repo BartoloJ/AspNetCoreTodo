8725025247d7b15e0779f02bea023b87baeb1268
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-11-05 15:59:45 -0800

    Added Mark Done

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Services;
 using AspNetCoreTodo.Models;
 
+
 namespace AspNetCoreTodo.Controllers
 {
 public class TodoController : Controller
@@ -30,5 +31,41 @@ public class TodoController : Controller
 
     return View(model);
     }
+
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> AddItem(TodoItem newItem)
+    {
+    if (!ModelState.IsValid)
+    {
+        return RedirectToAction("Index");
+    }
+
+    var successful = await _todoItemService.AddItemAsync(newItem);
+    if (!successful)
+    {
+        return BadRequest("Could not add item.");
+    }
+
+    return RedirectToAction("Index");
+    }
+    
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> MarkDone(Guid id)
+    {
+        if (id == Guid.Empty)
+        {
+            return RedirectToAction("Index");
+        }
+
+        var successful = await _todoItemService.MarkDoneAsync(id);
+        if (!successful)
+        {
+            return BadRequest("Could not mark item as done.");
+        }
+
+        return RedirectToAction("Index");
+    }
+    
+    }
 }
-}
+
--- AspNetCoreTodo/Services/ITodoItemService.cs
+++ AspNetCoreTodo/Services/ITodoItemService.cs
@@ -7,6 +7,11 @@ namespace AspNetCoreTodo.Services
 {
     public interface ITodoItemService
     {
+        Task<bool> AddItemAsync(TodoItem newItem);
         Task<TodoItem[]> GetIncompleteItemsAsync();
+
+        Task<bool> MarkDoneAsync(Guid id);
+    
     }
+    
 }
