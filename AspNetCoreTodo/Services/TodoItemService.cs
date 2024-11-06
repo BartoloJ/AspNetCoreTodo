551ccdd73d05ecd3e821ba4f78793e7323fdfb13
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-11-05 16:34:53 -0800

    Removed default value in services

--- AspNetCoreTodo/Services/TodoItemService.cs
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -28,7 +28,9 @@ namespace AspNetCoreTodo.Services
         {
             newItem.Id = Guid.NewGuid();
             newItem.IsDone = false;
-            newItem.DueAt = DateTimeOffset.Now.AddDays(3);
+
+
+
 
             _context.Items.Add(newItem);
 
