d7e69c67f5074848bc086bf4e874f2d4cf582a8d
Jonathan Bartolo <jonathan.bartolo@stmartin.edu>
2024-11-12 14:35:09 -0800

    Saved TodoController

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -48,7 +48,12 @@ namespace AspNetCoreTodo.Controllers
                 return RedirectToAction("Index");
             }
 
-            var successful = await _todoItemService.AddItemAsync(newItem);
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var successful = await _todoItemService
+                .AddItemAsync(newItem, currentUser);
+
             if (!successful)
             {
                 return BadRequest("Could not add item.");
@@ -65,7 +70,12 @@ namespace AspNetCoreTodo.Controllers
                 return RedirectToAction("Index");
             }
 
-            var successful = await _todoItemService.MarkDoneAsync(id);
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (currentUser == null) return Challenge();
+
+            var successful = await _todoItemService
+                .MarkDoneAsync(id, currentUser);
+
             if (!successful)
             {
                 return BadRequest("Could not mark item as done.");
